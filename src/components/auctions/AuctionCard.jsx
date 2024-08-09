0665c10db975b5fa8e6aa826cade381e18007536
fer <fer@fers-iMac.local>
2024-08-09 13:57:24 -0700

    comentarios

--- src/components/auctions/AuctionCard.jsx
+++ src/components/auctions/AuctionCard.jsx
@@ -65,16 +65,16 @@ export const AuctionCard = ({ items, UpdateById, UpdateByIdInventario }) => {
 
     const handleToggle =(id, el)=>{
 
-         if(noteState.length <= 0){
-                 alert('Falta el Numero Nota de Venta')
-                 return
-         }
+         // if(noteState.length <= 0){
+         //         alert('Falta el Numero Nota de Venta')
+         //         return
+         // }
 
          const obj = el
 
          obj.takenByCustomer = true
-         obj.historiSale = dueDate
-         obj.notaDeVenta = noteState
+         // obj.historiSale = dueDate
+         // obj.notaDeVenta = noteState
 
          UpdateById(id, obj)
 
@@ -84,70 +84,82 @@ export const AuctionCard = ({ items, UpdateById, UpdateByIdInventario }) => {
 
         //=====//================ Inventario  // 
 
-         obj.items.map((el,i)=>{
+         // obj.items.map((el,i)=>{
 
-                const a = itemsInventario.filter(ele => ele.id === el.id )[i]
+         //        const a = itemsInventario.filter(ele => ele.id === el.id )[i]
 
 
-                 if (a.historiSales === undefined) {
+         //         if (a.historiSales === undefined) {
 
-                         a.historiSales = [];
-                         a.historiSales.push(dueDate);
+         //                 a.historiSales = [];
+         //                 a.historiSales.push(dueDate);
 
-                         a.notaDeVenta = []
-                         a.notaDeVenta.push(noteState)
+         //                 a.notaDeVenta = []
+         //                 a.notaDeVenta.push(noteState)
 
-                         a.stockHermosillo = a?.stockHermosillo - el.quantity
+         //                 a.stockHermosillo = a?.stockHermosillo - el.quantity
 
-                         UpdateByIdInventario(a.id, a)
+         //                 UpdateByIdInventario(a.id, a)
 
-                 } else {
+         //         } else {
 
-                         a.historiSales.push(dueDate);
-                         a.notaDeVenta.push(noteState)
+         //                 a.historiSales.push(dueDate);
+         //                 a.notaDeVenta.push(noteState)
 
-                         a.stockHermosillo = a?.stockHermosillo - el.quantity
+         //                 a.stockHermosillo = a?.stockHermosillo - el.quantity
 
-                         UpdateByIdInventario(el.id, a)
+         //                 UpdateByIdInventario(el.id, a)
 
-                 }
+         //         }
    
             
-         })
+         // })
 
 
 
 
 
 
-        obj.items.map((el,i)=>{
+        // obj.items.map((el,i)=>{
 
-            const aa = itemsInventario.filter(ele => ele.id === el.id)[0]
-
-
-            for (let ind = 0; ind < Object.keys(tallaState).length ; ind++) {
-
-                    let everyValueArr = tallaState[Object.keys(tallaState)[ind]] // de la primer llave te da el primer valor y de la segunda llave te da el segundo objeto
+        //     const aa = itemsInventario.filter(ele => ele.id === el.id)[0]
 
 
-                    for (let i = 0; i < everyValueArr.length ; i++) {
-                            let b = aa.talla.filter(el => el !== everyValueArr[i])
-                            aa.talla = b
+        //     for (let ind = 0; ind < Object.keys(tallaState).length ; ind++) {
 
-                            UpdateByIdInventario(Object.keys(tallaState)[ind], aa)
+        //             let everyValueArr = tallaState[Object.keys(tallaState)[ind]] // de la primer llave te da el primer valor y de la segunda llave te da el segundo objeto
+
+
+        //             for (let i = 0; i < everyValueArr.length ; i++) {
+        //                     let b = aa.talla.filter(el => el !== everyValueArr[i])
+        //                     aa.talla = b
+
+        //                     UpdateByIdInventario(Object.keys(tallaState)[ind], aa)
        
-                    }   
+        //             }   
                    
 
-            }
+        //     }
             
-                setToggle(!toggle)
-            //console.log(Object.values(tallaState).flat())
+        //         setToggle(!toggle)
+        //     console.log(Object.values(tallaState).flat())
 
-        })
+        // })
+
+
+                //let a=['a','b']
+
+
+                //let b = ['a','b','c','d','e']
+
+                // a.map((el, i)=>{
+                //     let c = b.indexOf(el)
+                //     return b.splice(c,1)
+                // })
 
 
 
+                // b=['c', 'd', 'e']
 
     }
 
@@ -195,11 +207,11 @@ export const AuctionCard = ({ items, UpdateById, UpdateByIdInventario }) => {
                                 <b key={i}> 
                                 <h6>id {ele.id}</h6>
 
-                                    <img style={{width:'100px'}} src={ele.imgUrl}/><br />
+                                    <img style={{width:'100px'}} src={ele.imgUrl}/>
 
-                                    Cantidad: { ele.quantity} <br /> 
+                                     - Cantidad: { ele.quantity} <br /> 
 
-                                    <span className={el.takenByCustomer === true && 'd-none'}> Tallas: </span>
+                                   {/* <span className={el.takenByCustomer === true && 'd-none'}> Tallas: </span>
 
                                     {ele.talla.map((elem, i)=>(
                                             <b key={i+'talla'} className={el.takenByCustomer === true && 'd-none'}>{elem}, {''}</b>
@@ -209,14 +221,14 @@ export const AuctionCard = ({ items, UpdateById, UpdateByIdInventario }) => {
                                     <input className={el.takenByCustomer === true ? 'd-none' : 'c-r'}
                                             type="text" placeholder='Talla Escogida' 
                                             name={ele.id} value={tallaState[ele.id]}
-                                            onChange={(e)=>handleTallaState(e)}/><br /><br /><br />
+                                            onChange={(e)=>handleTallaState(e)}/><br /><br /><br />*/}
                                 </b>
                             ))}
 
 
 
 
-                           
+                           <br />
 
                             <p>Correo: {el.buyer.email}</p>
                             <p>Celular: {el.buyer.phone}</p>
@@ -231,23 +243,23 @@ export const AuctionCard = ({ items, UpdateById, UpdateByIdInventario }) => {
 
                            
 
-                            {
+                            {/*{
                                 el.takenByCustomer === true &&
                                     <>
                                             <p className='mt-2'> Pagado el : <b>{milisegundosComoFecha(el?.historiSale || 0)}</b></p>
                                             <p> Nota de Venta : <b>No. {el.notaDeVenta}</b></p>
                                     </>
-                            }
+                            }*/}
 
                         </div>
-
+{/*
                             {
                                 el.takenByCustomer === false &&
                                     <>
                                             <p className='m-0'>Status: <b>{el.takenByCustomer ? 'Entregado' : 'Pendiente'}</b></p>
                                             <input className='my-3 c-r' type="number" min='0' placeholder='# Nota de Venta' value={noteState} onChange={(e)=>setNoteState(e.target.value)}/>
                                     </>
-                            }
+                            }*/}
 
                            
 
@@ -255,7 +267,8 @@ export const AuctionCard = ({ items, UpdateById, UpdateByIdInventario }) => {
 
                         <button 
                             disabled={el.takenByCustomer === true ? true : false}
-                            className={el.takenByCustomer === true ? 'btn btn-outline-primary mt-0' : 'btn btn-primary mt-4' }  onClick={()=>handleToggle(el.id, el)}> 
+                            className={el.takenByCustomer === true ? 'btn btn-outline-primary mt-0' : 'btn btn-primary mt-4' }  
+                            onClick={()=>handleToggle(el.id, el)}> 
 
                                     {el.takenByCustomer ? 'Entregado' : 'Marcar como Entregada'}
                         </button>
